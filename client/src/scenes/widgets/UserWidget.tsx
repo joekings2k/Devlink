6f9827037dcb5b986500460411f1d902763c6852
jokings2k <eromoselejonthan@gmail.com>
2023-04-01 09:46:47 +0100

    few changes

--- client/src/scenes/widgets/UserWidget.tsx
+++ client/src/scenes/widgets/UserWidget.tsx
@@ -102,7 +102,7 @@ export const UserWidget = ({
         </Box>
         <Box display="flex" alignItems="center" gap="1rem">
           <WorkOutlineOutlined fontSize="large" sx={{ color: main }} />
-          <Typography color={medium}>{location}</Typography>
+          <Typography color={medium}>{occupation}</Typography>
         </Box>
       </Box>
       <Divider />
